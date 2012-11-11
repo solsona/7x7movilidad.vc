79e944ca6b9e515b4abd7e92e97810471c1796ad
Francisco Solsona <solsona@gmail.com>
2012-11-10 22:14:41 -0600

    Draft of presentation

---
+++ slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Google Maps',
+    subtitle: 'APIs y novedades - Nov 2012',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/chrome-logo-tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata',
+      'Cabin Sketch:400',
+      'Muli'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Francisco Solsona',
+    gplus: 'http://plus.ly/solsona',
+    title: 'Developer Relations Regional Lead',
+    company: 'Google',
+    twitter: '@solsona',
+    www: 'http://programa-con-google.blogspot.mx'
+  }]
+};
+
